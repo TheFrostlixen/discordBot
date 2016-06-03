6371c63d344f6960c977a583e88e90f5f2c58fb2
Aaron Whitehead <aaronbmwhitehead@gmail.com>
2016-06-03 15:07:26 -0700

    Forgot to commit changes whoops

--- run_bot.py
+++ run_bot.py
@@ -2,10 +2,12 @@ import discord
 import asyncio
 import requests
 import configparser
+import random
 
 import logging
 
 logging.basicConfig(level=logging.INFO)
+random.seed()
 
 #create the client object
 client = discord.Client()
@@ -13,16 +15,20 @@ config = configparser.ConfigParser()
 
 config.read("botConfig")
 
+LUNCH_OPTIONS = ["In-N-Out", "Panda Express", "Canes", "Chipotle", "McDonalds", "Jack in the Box"]
 
 token = config.get('Account', 'token')
 print(discord.__version__)
 
 @client.event
 async def on_message(message):
-    print("Asdf")
     if message.content.startswith('!test'):
-        print("asdfasdf")
-        await client.send_message(message.channel, '%s, hello!' % message.author)
+        await client.send_message(message.channel, '@%s, hello!' % message.author)
+    if message.content.startswith('!roll'):        
+        await client.send_message(message.channel, '%s' % random.randint(0,100))
+    if message.content.startswith('!lunch'):
+        temp = random.randint(0, len(LUNCH_OPTIONS) -1)
+        await client.send_message(message.channel, 'I think you should go to %s for lunch.' % LUNCH_OPTIONS[temp])
         
 @client.event
 async def on_ready():
