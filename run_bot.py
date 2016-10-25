e50bc8e844a6f5533cc7eb872ff8921567d7ed9e
Matt Fredrickson <mgfredrickson@nevada.unr.edu>
2016-10-25 16:20:50 -0700

    Add example code from Rapptz

--- run_bot.py
+++ run_bot.py
@@ -1,46 +1,31 @@
 import discord
 import asyncio
+from configparser import ConfigParser
 import requests
-import configparser
-import random
-
 import logging
 
-logging.basicConfig(level=logging.INFO)
-random.seed()
+#logging.basicConfig(level=logging.INFO)
+print("Using DISCORD.PY v" + discord.__version__)
 
 #create the client object
 client = discord.Client()
-config = configparser.ConfigParser()
 
+config = ConfigParser()
 config.read("bot.config")
-
-LUNCH_OPTIONS = ["In-N-Out", "Panda Express", "Canes", "Chipotle", "McDonalds", "Jack in the Box"]
-
 token = config.get('Account', 'token')
-print(discord.__version__)
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        await client.send_message(message.channel, '@%s, hello!' % message.author)
-    if message.content.startswith('!roll'):        
-        await client.send_message(message.channel, '%s' % random.randint(0,100))
-    if message.content.startswith('!lunch'):
-        temp = random.randint(0, len(LUNCH_OPTIONS) -1)
-        await client.send_message(message.channel, 'I think you should go to %s for lunch.' % LUNCH_OPTIONS[temp])
-        
+	if message.content.startswith('!test'):
+		await client.send_message(message.channel, '@%s, hello!' % message.author)
+	elif message.content.startswith('!status'):
+		await client.send_message(message.channel, 'Online and fully operational.')
+		
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
 
 client.run(token)
-
-
-
-
-
